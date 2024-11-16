557607cc4a97704c2b055ef7b4df57174730de2e
Vladimir Voinea <voineavladimir@gmail.com>
2024-11-16 19:56:37 +0200

    add tmux config and bash_profile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-ln -s .bash_profile ~/.bash_profile
-ln -s nvim ${XDG_CONFIG_HOME}/nvim
-ln -s .tmux.conf ~/.tmux.conf
+XDG_CONFIG_HOME=~/.config
+
+ln -s $(realpath .bash_profile) ~/.bash_profile
+ln -s $(realpath nvim) "$XDG_CONFIG_HOME/nvim"
+ln -s $(realpath .tmux.conf) ~/.tmux.conf
