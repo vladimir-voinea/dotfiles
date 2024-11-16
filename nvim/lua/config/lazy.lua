616d38764203528964aa43ba399cb70a62420012
Vladimir Voinea <voineavladimir@gmail.com>
2024-11-16 22:40:07 +0200

    add copilot

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
+      { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -34,7 +34,7 @@ require("lazy").setup({
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
-  },                -- automatically check for plugin updates
+  }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
@@ -52,22 +52,22 @@ require("lazy").setup({
   },
 })
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', function()
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", function()
   builtin.find_files({
-    cwd = vim.fn.getcwd(),   -- Set the current working directory
-    hidden = true,           -- Include hidden files
+    cwd = vim.fn.getcwd(), -- Set the current working directory
+    hidden = true, -- Include hidden files
     file_ignore_patterns = { -- Ignore specific directories and files
       "%.git/",
       "%.cargo/",
       "node_modules/",
       "%.cache/",
       "%.class",
-      "target"
+      "target",
     },
   })
 end, {})
 
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
